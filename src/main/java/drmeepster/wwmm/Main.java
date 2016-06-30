1d185918659cf7d5ae1ca31a0eddef5515f0727d
DrMeepster <mgonikberg@yahoo.com>
2016-06-29 21:48:18 -0700

    Finalization
    Getting ready to release a new verison

--- src/main/java/drmeepster/wwmm/Main.java
+++ src/main/java/drmeepster/wwmm/Main.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class Main{
 	
     public static final String MODID = "wwmm";
-    public static final String VERSION = "Alpha Build 1";
+    public static final String VERSION = "0.0.2.0";
     public static Block hardTaffy;
    
     @EventHandler
