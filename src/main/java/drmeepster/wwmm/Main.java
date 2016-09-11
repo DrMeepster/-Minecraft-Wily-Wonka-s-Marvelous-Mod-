55fd57b5e8c2a0b690cab0d09a56f9e036341027
DrMeepster <mgonikberg@yahoo.com>
2016-09-11 11:30:49 -0700

    reinstall

--- src/main/java/drmeepster/wwmm/Main.java
+++ src/main/java/drmeepster/wwmm/Main.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class Main{
 	
     public static final String MODID = "wwmm";
-    public static final String VERSION = "0.0.2.0";
+    public static final String VERSION = "0.1.0.0";
    
     @EventHandler
     public void PreInit(FMLPreInitializationEvent event){
